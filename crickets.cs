adff6d11a92d9a64e5c1e66463ef18175508dcb9
JISecond <115889759+JISecond@users.noreply.github.com>
2024-08-31 04:50:42 +0300

    Update crickets.cs

--- crickets.cs
+++ crickets.cs
@@ -4,13 +4,11 @@ public class DisplayCrickets : MonoBehaviour
 {
     void Start()
     {
-        // Выводим слово в лог при старте игры
         Debug.Log("crickets");
     }
 
     void Update()
     {
-        // Выводим слово в лог каждый кадр (если нужно)
         Debug.Log("crickets");
     }
 }
