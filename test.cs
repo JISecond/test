14e86faa63b7fe6f5d2022818f2c874fa564ec0d
JISecond <115889759+JISecond@users.noreply.github.com>
2024-08-31 04:48:45 +0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,16 @@
+using UnityEngine;
 
+public class DisplayCrickets : MonoBehaviour
+{
+    void Start()
+    {
+        // Выводим слово в лог при старте игры
+        Debug.Log("crickets");
+    }
+
+    void Update()
+    {
+        // Выводим слово в лог каждый кадр (если нужно)
+        Debug.Log("crickets");
+    }
+}
